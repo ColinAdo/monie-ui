ae17273a437c6e95965643682ded0ac1fd115b8d
ColinAdo <colinadodev30@gmail.com>
2024-09-05 10:34:58 +0300

    Add password reset confirm

--- components/forms/index.ts
+++ components/forms/index.ts
@@ -3,3 +3,4 @@ export { default as Input } from "./Input";
 export { default as LoginForm } from "./LoginForm";
 export { default as RegisterForm } from "./RegisterForm";
 export { default as ResetPasswordForm } from "./ResetPasswordForm";
+export { default as PasswordResetConfirmForm } from "./PasswordResetConfirmForm";
