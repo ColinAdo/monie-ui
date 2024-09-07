98e50bdcf98932f39770e2859bb93b543fc770f9
ColinAdo <colinadodev30@gmail.com>
2024-09-07 12:07:42 +0300

    Add sign in with social auth

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { LoginForm } from "@/components/forms";
+import { SocialButtons } from "@/components/common";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -23,6 +24,10 @@ export default function Page() {
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <LoginForm />
+        <span className="flex justify-center items-center mt-4 font-bold">
+          Or
+        </span>
+        <SocialButtons />
         <p className="mt-10 text-center text-sm text-gray-500">
           Don&apos;t have an account?{" "}
           <Link
