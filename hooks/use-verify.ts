defafb2cd84adb796d0b07d34eb2c8a7470b74c0
ColinAdo <colinadodev30@gmail.com>
2024-09-07 10:12:11 +0300

    Add proper use verify hook

--- hooks/use-verify.ts
+++ hooks/use-verify.ts
@@ -1,25 +1,3 @@
-// import { useEffect } from "react";
-// import { useAppDispatch } from "@/redux/hooks";
-// import { setAuth, finishInitialLoad } from "@/redux/features/authSlice";
-// import { useVerifyMutation } from "@/redux/features/authApiSlice";
-
-// export default function useVerify() {
-//   const dispatch = useAppDispatch();
-
-//   const [verify] = useVerifyMutation();
-
-//   useEffect(() => {
-//     verify(undefined)
-//       .unwrap()
-//       .then(() => {
-//         dispatch(setAuth());
-//       })
-//       .finally(() => {
-//         dispatch(finishInitialLoad());
-//       });
-//   }, []);
-// }
-
 import { useEffect } from "react";
 import { useAppDispatch } from "@/redux/hooks";
 import { setAuth, finishInitialLoad } from "@/redux/features/authSlice";
@@ -27,6 +5,7 @@ import { useVerifyMutation } from "@/redux/features/authApiSlice";
 
 export default function useVerify() {
   const dispatch = useAppDispatch();
+
   const [verify] = useVerifyMutation();
 
   useEffect(() => {
@@ -35,11 +14,8 @@ export default function useVerify() {
       .then(() => {
         dispatch(setAuth());
       })
-      .catch((error) => {
-        console.error("JWT verification failed:", error);
-      })
       .finally(() => {
         dispatch(finishInitialLoad());
       });
-  }, [dispatch, verify]);
+  }, []);
 }
