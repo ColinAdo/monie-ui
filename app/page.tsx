cff74911f95e08a15a873aea9fe0f02d99673198
ColinAdo <colinadodev30@gmail.com>
2024-09-07 10:11:53 +0300

    Add proper home page

--- app/page.tsx
+++ app/page.tsx
@@ -8,20 +8,8 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <div className="bg-white">
+    <main className="bg-white">
       <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div
-          aria-hidden="true"
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        >
-          <div
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          />
-        </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
             <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-6xl">
@@ -31,9 +19,7 @@ export default function Page() {
               Our Money Tracker application is designed to help you take control
               of your finances effortlessly. Track your income, expenses, and
               savings in real-time, while gaining insights into your spending
-              habits. With a user-friendly interface, powerful budgeting tools,
-              and secure data management, managing your money has never been
-              easier. Stay organized, set financial goals, and make informed
+              habits. Stay organized, set financial goals, and make informed
               decisions with Money Tracker.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
@@ -65,6 +51,6 @@ export default function Page() {
           />
         </div>
       </div>
-    </div>
+    </main>
   );
 }
