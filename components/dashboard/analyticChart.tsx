f9a05bfb0c3fbbe1a5053c836c3fe81823881249
ColinAdo <colinadodev30@gmail.com>
2024-11-07 10:48:25 +0300

    Add Line chart component

---
+++ components/dashboard/analyticChart.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  ResponsiveContainer,
+} from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import data from "@/data/analytics";
+
+export default function AnalyticCard() {
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Post analytics</CardTitle>
+          <CardDescription>
+            Line Chart showing analytic for post
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div style={{ width: "100%", height: 300 }}>
+            <ResponsiveContainer>
+              <LineChart width={1100} height={300} data={data}>
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                <CartesianGrid type="monotone" stroke="#ccc" />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  );
+}
--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -1,3 +1,4 @@
 export { default as PageTitle } from "./PageTitile";
 export { default as SalesCard } from "./SalesCard";
+export { default as AnalyticChart } from "./analyticChart";
 export { default as Chart } from "./Chart";
