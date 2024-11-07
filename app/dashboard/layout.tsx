1931f623e9202a5a038b57fe663cb2e3db2e05a8
ColinAdo <colinadodev30@gmail.com>
2024-11-07 10:22:18 +0300

    Add proper dashboard code

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,20 @@
 import { RequireAuth } from "@/components/utils";
+import { Navbar, Sidebar } from "@/components/common";
 
 interface Props {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: Props) {
-  return <RequireAuth>{children}</RequireAuth>;
+  return (
+    <RequireAuth>
+      <Navbar />
+      <div className="flex">
+        <div className="hidden md:block">
+          <Sidebar />
+        </div>
+        <div className="p-5 w-full max-w-[1140px]">{children}</div>
+      </div>
+    </RequireAuth>
+  );
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,48 +1,107 @@
-"use client";
+import Card, { CardProps } from "@/components/dashboard/Card";
+import { CardContent } from "@/components/dashboard/Card";
+import { PageTitle, Chart } from "@/components/dashboard";
+import { Activity, CreditCard, DollarSign, Users } from "lucide-react";
+import SalesCard, { SalesProps } from "@/components/dashboard/SalesCard";
 
-import { useRouter } from "next/navigation";
-import { List, Spinner } from "@/components/common";
-import { useRetrieveUserQuery } from "@/redux/features/authApiSlice";
-import { toast } from "react-toastify";
+const CardData: CardProps[] = [
+  {
+    lebal: "Sales",
+    icon: CreditCard,
+    amount: "+246,340.00",
+    description: "Sales for the whole month",
+  },
+  {
+    lebal: "Users",
+    icon: Users,
+    amount: "+24,000.00",
+    description: "Users signed up this month",
+  },
+  {
+    lebal: "Activity",
+    icon: Activity,
+    amount: "-10,000.00",
+    description: "Website engagement with the whole month",
+  },
+  {
+    lebal: "Total Revenue",
+    icon: DollarSign,
+    amount: "+350,000.00",
+    description: "Revenue of the whole month",
+  },
+];
 
-export default function Page() {
-  const router = useRouter();
-  const { data: user, isLoading, isError } = useRetrieveUserQuery();
-  const config = [
-    {
-      label: "Username",
-      value: user?.username,
-    },
-    {
-      label: "Email",
-      value: user?.email,
-    },
-  ];
+const SalesData: SalesProps[] = [
+  {
+    name: "Annie",
+    email: "annie@gmail.com",
+    saleAmount: "+567,000.00",
+  },
+  {
+    name: "Maggie",
+    email: "maggie@gmail.com",
+    saleAmount: "-100,000.00",
+  },
+  {
+    name: "George",
+    email: "george@gmail.com",
+    saleAmount: "+450,000.00",
+  },
+  {
+    name: "Coco",
+    email: "coco@gmail.com",
+    saleAmount: "+900,000.00",
+  },
+  {
+    name: "Boo",
+    email: "boo@gmail.com",
+    saleAmount: "-50,000.00",
+  },
+  {
+    name: "Sasha",
+    email: "sasha@gmail.com",
+    saleAmount: "+370,000.00",
+  },
+];
 
-  if (isLoading) {
-    return (
-      <div className="flex justify-center my-8">
-        <Spinner lg />
-      </div>
-    );
-  }
-
-  if (isError) {
-    toast.error("Please login");
-    router.push("/auth/login");
-  }
+export default function Home() {
   return (
-    <>
-      <header className="bg-white shadow">
-        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-            Dashboard
-          </h1>
-        </div>
-      </header>
-      <main className="mx-auto max-w-7xl py-6 my-8 sm:px-6 lg:px-8">
-        <List config={config} />
-      </main>
-    </>
+    <div className="flex flex-col gap-5 w-full">
+      <PageTitle title="Dasboard" />
+      <section className="grid w-full grid-cols-1 gap-2 sm:grid-cols-2 xl:grid-cols-4 transition-all">
+        {CardData.map((d, i) => (
+          <Card
+            key={i}
+            amount={d.amount}
+            icon={d.icon}
+            lebal={d.lebal}
+            description={d.description}
+          />
+        ))}
+      </section>
+
+      <section className="grid grid-cols-1 gap-4 lg:grid-cols-2 transition-all">
+        <CardContent>
+          <p className="p-4 font-semibold">Overview</p>
+          <Chart />
+        </CardContent>
+        <CardContent>
+          <section>
+            <p className="font-semibold">Sales</p>
+            <p className="text-gray-500 text-sm">
+              You have made about 567 sales this month
+            </p>
+            {SalesData.map((d, i) => (
+              <SalesCard
+                key={i}
+                name={d.name}
+                email={d.email}
+                saleAmount={d.saleAmount}
+              />
+            ))}
+          </section>
+        </CardContent>
+      </section>
+    </div>
   );
 }
