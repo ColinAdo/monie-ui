56a77811c26332af81d251f473175034f78b6662
ColinAdo <colinadodev30@gmail.com>
2024-11-08 10:25:37 +0300

    Add proper auth components

---
+++ components/auth/AuthTabs.tsx
@@ -0,0 +1,20 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import LoginForm from "./LoginForm";
+import RegisterForm from "./RegisterForm";
+
+export default function AuthTabs() {
+  return (
+    <Tabs defaultValue="signIn" className="w-[400px] font-mono">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="signIn">Sign In</TabsTrigger>
+        <TabsTrigger value="signUp">Sign Up</TabsTrigger>
+      </TabsList>
+      <TabsContent value="signIn">
+        <LoginForm />
+      </TabsContent>
+      <TabsContent value="signUp">
+        <RegisterForm />
+      </TabsContent>
+    </Tabs>
+  );
+}
---
+++ components/auth/LoginForm.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import * as z from "zod";
+import { loginSchema } from "@/lib/schemas";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import SocialButtons from "@/components/auth/SocialButtons";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+export default function LoginForm() {
+  const router = useRouter();
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleSubmit = (data: z.infer<typeof loginSchema>) => {
+    router.push("/");
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Sign in</CardTitle>
+        <CardDescription>Sign in into your account</CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="space-y-6"
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-white">
+                    Email
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter Email"
+                      {...field}
+                      className="bg-slate-100 dark:bg-slate-500 text-blak dark:text-slate-100 focus-visible:ring-0 focus-visible:ring-offset-0"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-white">
+                    Password
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Enter Password"
+                      {...field}
+                      className="bg-slate-100 dark:bg-slate-500 text-blak dark:text-slate-100 focus-visible:ring-0 focus-visible:ring-offset-0"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button className="w-full dark:text-white dark:bg-slate-800">
+              Submit
+            </Button>
+          </form>
+          <div className="relative flex items-center my-3 font-bold">
+            <span className="flex-grow border-t border-gray-950 dark:border-gray-300"></span>
+            <span className="mx-3">OR</span>
+            <span className="flex-grow border-t border-gray-950 dark:border-gray-300"></span>
+          </div>
+          <SocialButtons />
+        </Form>
+      </CardContent>
+    </Card>
+  );
+}
