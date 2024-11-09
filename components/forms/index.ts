dd0539cc7e3dc5c93a5ccc1f9c5d4869014ea928
ColinAdo <colinadodev30@gmail.com>
2024-11-09 08:59:55 +0300

    Removed old code

--- components/forms/index.ts
+++ components/forms/index.ts
@@ -1,6 +1,4 @@
 export { default as Form } from "./Form";
 export { default as Input } from "./Input";
-export { default as LoginForm } from "./LoginForm";
 export { default as RegisterForm } from "./RegisterForm";
-export { default as ResetPasswordForm } from "./ResetPasswordForm";
 export { default as PasswordResetConfirmForm } from "./PasswordResetConfirmForm";
