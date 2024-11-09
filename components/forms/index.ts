ec7aaa43fde806820a6d9b2e071fdddfd868f2b0
ColinAdo <colinadodev30@gmail.com>
2024-11-09 09:18:00 +0300

    Removed old password reset confirm

--- components/forms/index.ts
+++ components/forms/index.ts
@@ -1,4 +1,3 @@
 export { default as Form } from "./Form";
 export { default as Input } from "./Input";
 export { default as RegisterForm } from "./RegisterForm";
-export { default as PasswordResetConfirmForm } from "./PasswordResetConfirmForm";
