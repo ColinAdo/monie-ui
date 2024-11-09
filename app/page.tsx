710ee94d446fa04db1439002716b91b2e5622aca
ColinAdo <colinadodev30@gmail.com>
2024-11-09 09:45:03 +0300

    Add proper link to login page

--- app/page.tsx
+++ app/page.tsx
@@ -24,16 +24,10 @@ export default function Page() {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
-                href="/auth/register"
+                href="/auth/login"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Get started
-              </Link>
-              <Link
-                href="/auth/login"
-                className="text-sm font-semibold leading-6 text-gray-900"
-              >
-                Login <span aria-hidden="true">&rarr;</span>
+                Get started <span aria-hidden="true">&rarr;</span>
               </Link>
             </div>
           </div>
