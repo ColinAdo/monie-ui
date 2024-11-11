51ef673876f3bd558fec08168a2fe37ed3adc132
ColinAdo <colinadodev30@gmail.com>
2024-11-11 09:08:27 +0300

    Add proper icon classNames

--- components/common/ThemeToggler.tsx
+++ components/common/ThemeToggler.tsx
@@ -18,8 +18,8 @@ export default function ThemeToggler() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size={null} className=" border-0">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all  dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:mr-14 dark:rotate-0 dark:scale-100" />
           Theme
           <span className="sr-only">Toggle theme</span>
         </Button>
