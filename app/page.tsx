819c04f34d5b5086992d5fd5e3bbcadff2715f2c
ColinAdo <colinadodev30@gmail.com>
2024-11-12 09:39:50 +0300

    Add style home page

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className="bg-white dark:bg-zinc-950">
-      <div className="relative isolate px-6 pt-14 lg:px-8">
+      <div className="relative isolate px-6 lg:px-8">
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
             <h1 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-6xl">
