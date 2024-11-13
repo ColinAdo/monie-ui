e8df654a79527208ee90487e7398dd6fb68b8fff
ColinAdo <colinadodev30@gmail.com>
2024-11-13 08:56:12 +0300

    Add edit account form

--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -4,3 +4,4 @@ export { default as AnalyticChart } from "./analyticChart";
 export { default as Chart } from "./Chart";
 export { default as FormDialog } from "./FormDialog";
 export { default as CreateAccountForm } from "./CreateAccountForm";
+export { default as EditAccountForm } from "./EditAccountForm";
