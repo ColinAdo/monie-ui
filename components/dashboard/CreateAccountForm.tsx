b12eb0ad936d6a92668d67e191a298b5800257a9
ColinAdo <colinadodev30@gmail.com>
2024-11-13 08:42:54 +0300

    Add dialog form

---
+++ components/dashboard/CreateAccountForm.tsx
@@ -0,0 +1,49 @@
+import { Button } from "@/components/ui/button";
+import { DialogFooter } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function CreateAccountForm() {
+  return (
+    <div>
+      <form>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Account name
+            </Label>
+            <Input
+              id="name"
+              defaultValue="Pedro Duarte"
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="description" className="text-right">
+              Description
+            </Label>
+            <Input
+              id="description"
+              defaultValue="Account for managing airtime"
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="amount" className="text-right">
+              Amount
+            </Label>
+            <Input
+              id="amount"
+              type="number"
+              defaultValue="50000"
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Submit</Button>
+        </DialogFooter>
+      </form>
+    </div>
+  );
+}
--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -2,3 +2,5 @@ export { default as PageTitle } from "./PageTitile";
 export { default as SalesCard } from "./SalesCard";
 export { default as AnalyticChart } from "./analyticChart";
 export { default as Chart } from "./Chart";
+export { default as FormDialog } from "./FormDialog";
+export { default as CreateAccountForm } from "./CreateAccountForm";
