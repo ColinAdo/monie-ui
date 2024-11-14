a5afb5e1e85e67e1b8c01f4ac552444149c08bb9
ColinAdo <colinadodev30@gmail.com>
2024-11-14 09:57:14 +0300

    Add create transaction form

--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -4,4 +4,5 @@ export { default as AnalyticChart } from "./analyticChart";
 export { default as Chart } from "./Chart";
 export { default as FormDialog } from "./FormDialog";
 export { default as CreateAccountForm } from "./CreateAccountForm";
+export { default as CreateTransactionForm } from "./CreateTransactionForm";
 export { default as EditAccountForm } from "./EditAccountForm";
