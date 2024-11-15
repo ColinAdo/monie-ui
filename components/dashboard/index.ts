ded093f7bac742bdaf7f5a1b05dd44d1cb57a738
ColinAdo <colinadodev30@gmail.com>
2024-11-15 21:25:39 +0300

    Add proper arranged import

--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -1,8 +1,8 @@
-export { default as PageTitle } from "./PageTitile";
-export { default as SalesCard } from "./SalesCard";
-export { default as AnalyticChart } from "./analyticChart";
 export { default as Chart } from "./Chart";
+export { default as SalesCard } from "./SalesCard";
+export { default as PageTitle } from "./PageTitile";
 export { default as FormDialog } from "./FormDialog";
+export { default as AnalyticChart } from "./analyticChart";
+export { default as EditAccountForm } from "./EditAccountForm";
 export { default as CreateAccountForm } from "./CreateAccountForm";
 export { default as CreateTransactionForm } from "./CreateTransactionForm";
-export { default as EditAccountForm } from "./EditAccountForm";
