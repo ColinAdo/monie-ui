2250111fd74b4f24335be482cbaac311444467e7
ColinAdo <colinadodev30@gmail.com>
2024-12-07 18:02:22 +0300

    Add proper import

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
+import type { Metadata } from "next";
 import { RegisterForm } from "@/components/forms";
 import { SocialButtons } from "@/components/common";
-import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Monie | Register",
