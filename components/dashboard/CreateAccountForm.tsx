4c0305d7105643593cd29987cdffc62eaea1a32c
ColinAdo <colinadodev30@gmail.com>
2024-12-11 13:26:22 +0300

    Add proper create account

--- components/dashboard/CreateAccountForm.tsx
+++ components/dashboard/CreateAccountForm.tsx
@@ -1,49 +1,139 @@
+"use client";
+
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import { DialogFooter } from "@/components/ui/dialog";
+import useWebSocket from "react-use-websocket";
+import { useEffect, useState, useRef, ChangeEvent, FormEvent } from "react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { accountSchema } from "@/lib/schemas";
+import { Spinner } from "@/components/common";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Card, CardContent } from "@/components/ui/card";
+import { useAppSelector } from "@/redux/hooks";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 export default function CreateAccountForm() {
+  const router = useRouter();
+  const { isLoading } = useAppSelector((state) => state.auth);
+  const WS_URL = `${process.env.NEXT_PUBLIC_WS_HOST}/api/v1/accounts/`;
+
+  const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(
+    WS_URL,
+    {
+      share: false,
+      shouldReconnect: () => true,
+    }
+  );
+
+  useEffect(() => {
+    console.log("Connection state changed", readyState);
+  }, [readyState]);
+
+  const form = useForm<z.infer<typeof accountSchema>>({
+    resolver: zodResolver(accountSchema),
+    defaultValues: {
+      name: "",
+      description: "",
+      amount: "",
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof accountSchema>) => {
+    sendJsonMessage(data);
+    toast.success("Account created successfully");
+    router.push("/dashboard");
+  };
+
   return (
-    <div>
-      <form>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Account name
-            </Label>
-            <Input
-              id="name"
-              defaultValue="Pedro Duarte"
-              className="col-span-3"
+    <Card>
+      <CardContent className="space-y-2">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-white">
+                    Account name
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter account name"
+                      {...field}
+                      className=" dark:bg-zinc-950 text-blak dark:text-slate-100 focus-visible:ring-0 focus-visible:ring-offset-0"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="description" className="text-right">
-              Description
-            </Label>
-            <Input
-              id="description"
-              defaultValue="Account for managing airtime"
-              className="col-span-3"
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="flex justify-between items-center">
+                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-white">
+                      Account description
+                    </FormLabel>
+                  </div>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      placeholder="Enter account description"
+                      {...field}
+                      className="dark:bg-zinc-950  text-blak dark:text-slate-100 focus-visible:ring-0 focus-visible:ring-offset-0"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="amount" className="text-right">
-              Amount
-            </Label>
-            <Input
-              id="amount"
-              type="number"
-              defaultValue="50000"
-              className="col-span-3"
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="flex justify-between items-center">
+                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-white">
+                      Account Amount
+                    </FormLabel>
+                  </div>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      placeholder="Enter account amount"
+                      {...field}
+                      className="dark:bg-zinc-950  text-blak dark:text-slate-100 focus-visible:ring-0 focus-visible:ring-offset-0"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Submit</Button>
-        </DialogFooter>
-      </form>
-    </div>
+
+            <Button
+              className="w-full dark:text-black font-bold dark:bg-white"
+              disabled={isLoading}
+            >
+              {isLoading ? <Spinner sm /> : "Submit"}
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   );
 }
