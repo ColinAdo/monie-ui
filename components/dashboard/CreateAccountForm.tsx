036cb16d31f6d3722ded0c946168be87861b4f47
ColinAdo <colinadodev30@gmail.com>
2024-12-28 10:04:05 +0300

    Add proper form

--- components/dashboard/CreateAccountForm.tsx
+++ components/dashboard/CreateAccountForm.tsx
@@ -44,80 +44,78 @@ export default function CreateAccountForm() {
   };
 
   return (
-    <Card>
-      <CardContent className="space-y-2">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-white">
+                  Account name
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Enter account name"
+                    {...field}
+                    className=" dark:bg-zinc-950 text-blak dark:text-slate-100 focus-visible:ring-0 focus-visible:ring-offset-0"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <div className="flex justify-between items-center">
                   <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-white">
-                    Account name
+                    Account description
                   </FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Enter account name"
-                      {...field}
-                      className=" dark:bg-zinc-950 text-blak dark:text-slate-100 focus-visible:ring-0 focus-visible:ring-offset-0"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="description"
-              render={({ field }) => (
-                <FormItem>
-                  <div className="flex justify-between items-center">
-                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-white">
-                      Account description
-                    </FormLabel>
-                  </div>
-                  <FormControl>
-                    <Input
-                      type="text"
-                      placeholder="Enter account description"
-                      {...field}
-                      className="dark:bg-zinc-950  text-blak dark:text-slate-100 focus-visible:ring-0 focus-visible:ring-offset-0"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="amount"
-              render={({ field }) => (
-                <FormItem>
-                  <div className="flex justify-between items-center">
-                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-white">
-                      Account Amount
-                    </FormLabel>
-                  </div>
-                  <FormControl>
-                    <Input
-                      type="number"
-                      placeholder="Enter account amount"
-                      {...field}
-                      className="dark:bg-zinc-950  text-blak dark:text-slate-100 focus-visible:ring-0 focus-visible:ring-offset-0"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+                </div>
+                <FormControl>
+                  <Input
+                    type="text"
+                    placeholder="Enter account description"
+                    {...field}
+                    className="dark:bg-zinc-950  text-blak dark:text-slate-100 focus-visible:ring-0 focus-visible:ring-offset-0"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="amount"
+            render={({ field }) => (
+              <FormItem>
+                <div className="flex justify-between items-center">
+                  <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-white">
+                    Account Amount
+                  </FormLabel>
+                </div>
+                <FormControl>
+                  <Input
+                    type="number"
+                    placeholder="Enter account amount"
+                    {...field}
+                    className="dark:bg-zinc-950  text-blak dark:text-slate-100 focus-visible:ring-0 focus-visible:ring-offset-0"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-            <Button className="w-full dark:text-black font-bold dark:bg-white">
-              Submit
-            </Button>
-          </form>
-        </Form>
-      </CardContent>
-    </Card>
+          <Button className="w-full dark:text-black font-bold dark:bg-white">
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </>
   );
 }
