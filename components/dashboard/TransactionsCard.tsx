fc152a0446803527caa14521aa521ef82a10af7a
ColinAdo <colinadodev30@gmail.com>
2024-12-30 11:35:34 +0300

    Add proper transaction card

---
+++ components/dashboard/TransactionsCard.tsx
@@ -0,0 +1,37 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Timestamp } from "@/components/dashboard";
+
+interface Props {
+  create_at: Date;
+  amount: number;
+  username: string;
+  accountName: string;
+  description: string;
+  transactionType: string
+}
+
+export default function TransactionsCard(props: Props) {
+  return (
+    <div className="flex flex-wrap justify-between gap-3">
+      <section className="flex justify-between gap-3 p-2">
+        <Avatar>
+          <AvatarFallback className="text-black bg-slate-300 font-bold">
+            {props.username[0]}
+          </AvatarFallback>
+        </Avatar>
+        <div className="text-sm">
+          <p>{props.accountName}</p>
+          <div className="hidden sm:block text-ellipsis overflow-hidden whitespace-nowrap w-[120px] sm:w-auto text-gray-500">
+            <p>{props.description}</p>
+          </div>
+        </div>
+      </section>
+      <div className="flex flex-col items-center">
+        <span>{props.amount}</span>
+        <span className="hidden sm:block text-ellipsis overflow-hidden whitespace-nowrap w-[120px] sm:w-auto text-gray-500">
+          <Timestamp createdAt={props.create_at} />
+        </span>
+      </div>
+    </div>
+  );
+}
