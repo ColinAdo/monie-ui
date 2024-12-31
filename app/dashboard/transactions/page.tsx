a9b2952739601c45f46b093d90fcd8b13280b8ae
ColinAdo <colinadodev30@gmail.com>
2024-12-31 16:14:42 +0300

    Add real time display of transaction

--- app/dashboard/transactions/page.tsx
+++ app/dashboard/transactions/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 
+import { useEffect } from "react";
 import { PageTitle } from "@/components/dashboard";
-import { columns } from "@/components/transactions/Columns"
+import { columns } from "@/components/transactions/columns"
 import { DataTable } from "@/components/transactions/data-table"
 import { useGetTransactionsQuery } from "@/redux/features/accountSlice";
+import { useWebSocketContext } from "@/hooks/WebSocketContext";
 
 export default function Page() {
-    const { data: transactions } = useGetTransactionsQuery();
+    const { lastJsonMessage } = useWebSocketContext();
+    const { data: transactions, refetch } = useGetTransactionsQuery();
+
+    useEffect(() => {
+        refetch();
+    }, [lastJsonMessage]);
 
     if (!transactions) {
         return
