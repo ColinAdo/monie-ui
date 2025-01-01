41d385c64300712abfc55b253be69dadee0d7747
ColinAdo <colinadodev30@gmail.com>
2025-01-01 10:42:10 +0300

    Add Analytic type

--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -4,6 +4,7 @@ export { default as Timestamp } from "./Timestamp";
 export { default as PageTitle } from "./PageTitile";
 export { default as FormDialog } from "./FormDialog";
 export { default as AnalyticChart } from "./analyticChart";
+export { default as AnalyticPieChart } from "./AnalyticPieChart";
 export { default as EditAccountForm } from "./EditAccountForm";
 export { default as CreateAccountForm } from "./CreateAccountForm";
 export { default as CreateTransactionForm } from "./CreateTransactionForm";
