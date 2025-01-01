2ed7a902c863e2b73cb18d8e69164d80e03e4497
ColinAdo <colinadodev30@gmail.com>
2025-01-01 11:13:58 +0300

    Add proper file names

--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -1,9 +1,9 @@
-export { default as Chart } from "./Chart";
+export { default as Chart } from "./AnalyticBarChart";
 export { default as TransactionsCard } from "./TransactionsCard";
 export { default as Timestamp } from "./Timestamp";
 export { default as PageTitle } from "./PageTitile";
 export { default as FormDialog } from "./FormDialog";
-export { default as AnalyticChart } from "./analyticChart";
+export { default as AnalyticChart } from "./analyticLineChart";
 export { default as AnalyticPieChart } from "./AnalyticPieChart";
 export { default as EditAccountForm } from "./EditAccountForm";
 export { default as CreateAccountForm } from "./CreateAccountForm";
