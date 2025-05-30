ce956526fb2b22ef5ff87e9702f310899db041c8
ColinAdo <colinadodev30@gmail.com>
2025-05-30 08:28:27 +0300

    Add production ready code

--- app/auth/google/page.tsx
+++ app/auth/google/page.tsx
@@ -1,33 +1,12 @@
-"use client";
-
-import { useSocialAuth } from "@/hooks";
-import { Spinner } from "@/components/common";
-import { useSocialAuthenticateMutation } from "@/redux/features/authApiSlice";
-
-export default function Page() {
-  const [googleAuthenticate] = useSocialAuthenticateMutation();
-  useSocialAuth(googleAuthenticate, "google-oauth2");
-  return (
-    <div className="flex justify-center items-center my-8">
-      <Spinner lg />
-    </div>
-  );
-}
-
 // "use client";
 
-// export const dynamic = "force-dynamic";
-// export const fetchCache = "force-no-store";
-
-// import { Suspense } from "react";
 // import { useSocialAuth } from "@/hooks";
 // import { Spinner } from "@/components/common";
 // import { useSocialAuthenticateMutation } from "@/redux/features/authApiSlice";
 
-// function GoogleAuthPage() {
+// export default function Page() {
 //   const [googleAuthenticate] = useSocialAuthenticateMutation();
 //   useSocialAuth(googleAuthenticate, "google-oauth2");
-
 //   return (
 //     <div className="flex justify-center items-center my-8">
 //       <Spinner lg />
@@ -35,11 +14,32 @@ export default function Page() {
 //   );
 // }
 
-// export default function PageWrapper() {
-//   return (
-//     <Suspense fallback={<div className="flex justify-center items-center h-screen">Loading...</div>}>
-//       <GoogleAuthPage />
-//     </Suspense>
-//   );
-// }
+"use client";
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+import { Suspense } from "react";
+import { useSocialAuth } from "@/hooks";
+import { Spinner } from "@/components/common";
+import { useSocialAuthenticateMutation } from "@/redux/features/authApiSlice";
+
+function GoogleAuthPage() {
+  const [googleAuthenticate] = useSocialAuthenticateMutation();
+  useSocialAuth(googleAuthenticate, "google-oauth2");
+
+  return (
+    <div className="flex justify-center items-center my-8">
+      <Spinner lg />
+    </div>
+  );
+}
+
+export default function PageWrapper() {
+  return (
+    <Suspense fallback={<div className="flex justify-center items-center h-screen">Loading...</div>}>
+      <GoogleAuthPage />
+    </Suspense>
+  );
+}
 
