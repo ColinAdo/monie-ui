779084fc051a30dfb1ce242e55141455baf2a356
ColinAdo <colinadodev30@gmail.com>
2025-05-30 08:22:54 +0300

    Add proper import

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,4 @@
-import ContinueWithSocialAuth from "./continue-with-social-auth";
+import ContinueWithSocialAuth from "@/utils/continue-with-social-auth";
 
 export const continueWithGoogle = () =>
   ContinueWithSocialAuth("google-oauth2", "google");
