918b32116be2097889689fa927618c8a4f99939a
ColinAdo <colinadodev30@gmail.com>
2025-06-06 13:43:36 +0300

    Add proper width

--- components/auth/AuthTabs.tsx
+++ components/auth/AuthTabs.tsx
@@ -9,7 +9,7 @@ import {
 
 export default function AuthTabs() {
   return (
-    <Tabs defaultValue="signIn" className="w-[320px] sm:w-[300px] font-mono">
+    <Tabs defaultValue="signIn" className="lg:w-[400px] sm:w-[320px] font-mono">
       <TabsList className="grid w-full grid-cols-2">
         <TabsTrigger value="signIn">Sign In</TabsTrigger>
         <TabsTrigger value="signUp">Sign Up</TabsTrigger>
