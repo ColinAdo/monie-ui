f101dcba6efe9ff15f9e51e2f8cff5888210eaa3
ColinAdo <colinadodev30@gmail.com>
2025-06-08 15:46:13 +0300

    Add chat route

---
+++ app/dashboard/chat/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { ChatForm } from "@/components/dashboard";
+
+export default function Paage() {
+  return (
+    <div className="flex flex-col gap-5 w-full mt-10">
+      <ChatForm />
+    </div>
+  );
+}
