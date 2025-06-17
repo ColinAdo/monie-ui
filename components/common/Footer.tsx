8a68e5982b838ead47679492185be910c6aa113c
ColinAdo <colinadodev30@gmail.com>
2025-06-17 10:20:14 +0300

    Add proper footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -2,10 +2,11 @@ export default function Footer() {
   return (
     <footer className="h-16">
       <div className="h-full px-2">
-        <div className="flex items-center justify-center h-full">
+        <div className="flex flex-col items-center justify-center h-full">
           <p className="text-gray-500 text-xs">
-            &copy; 2024 Monie, Inc. All rights reserved.
+            &copy; 2025 Monie • All rights reserved.
           </p>
+          <p className="text-gray-500 text-xs">inbox@mail.colinadore.tech</p>
         </div>
       </div>
     </footer>
