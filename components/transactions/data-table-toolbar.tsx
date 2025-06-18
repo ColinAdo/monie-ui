440f6ba6907a59d5d3251aa288d4fd0bb72fb76e
ColinAdo <colinadodev30@gmail.com>
2025-06-18 09:03:17 +0300

    Add proper width

--- components/transactions/data-table-toolbar.tsx
+++ components/transactions/data-table-toolbar.tsx
@@ -1,71 +1,80 @@
-"use client"
+"use client";
 
-import { Input } from "@/components/ui/input"
-import { Table } from "@tanstack/react-table"
-import { Search, CircleX } from 'lucide-react'
-import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input";
+import { Table } from "@tanstack/react-table";
+import { Search, CircleX } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select"
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 interface DataTableToolbarProps<TData> {
-    table: Table<TData>
+  table: Table<TData>;
 }
 
 export function DataTableToolbar<TData>({
-    table,
+  table,
 }: DataTableToolbarProps<TData>) {
-    const isFiltered = table.getState().columnFilters.length > 0;
+  const isFiltered = table.getState().columnFilters.length > 0;
 
-    return (
-        <div className="flex items-center justify-between">
-            <div className="flex items-center space-x-2">
-                <div className="relative w-64">
-                    <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
-                    <Input
-                        placeholder="Search account..."
-                        value={(table.getColumn("account_name")?.getFilterValue() as string) ?? ""}
-                        onChange={(event) =>
-                            table.getColumn("account_name")?.setFilterValue(event.target.value)
-                        }
-                        className="pl-8"
-                    />
-                </div>
-            </div>
-
-            <div className="flex items-center space-x-2">
-                <Select
-                    value={(table.getColumn("transaction_type")?.getFilterValue() as string) ?? "all"}
-                    onValueChange={(value) =>
-                        table.getColumn("transaction_type")?.setFilterValue(value === "all" ? "" : value)
-                    }
-                >
-                    <SelectTrigger className="w-[180px]">
-                        <SelectValue placeholder="Select mode" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectItem value="all">All transactions</SelectItem>
-                        <SelectItem value="Income">Incomes</SelectItem>
-                        <SelectItem value="Expense">Expenses</SelectItem>
-                    </SelectContent>
-                </Select>
-                {isFiltered && (
-                    <Button
-                        variant="ghost"
-                        onClick={() => table.resetColumnFilters()}
-                        className="h-8 px-2 lg:px-3"
-                    >
-                        Reset
-                        <CircleX className="ml-2 h-4 w-4" />
-                    </Button>
-                )}
-            </div>
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center space-x-2">
+        <div className="relative w-48 sm:w-64">
+          <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+          <Input
+            placeholder="Search account..."
+            value={
+              (table.getColumn("account_name")?.getFilterValue() as string) ??
+              ""
+            }
+            onChange={(event) =>
+              table
+                .getColumn("account_name")
+                ?.setFilterValue(event.target.value)
+            }
+            className="pl-8"
+          />
         </div>
-    );
-}
+      </div>
 
+      <div className="flex items-center space-x-2">
+        <Select
+          value={
+            (table.getColumn("transaction_type")?.getFilterValue() as string) ??
+            "all"
+          }
+          onValueChange={(value) =>
+            table
+              .getColumn("transaction_type")
+              ?.setFilterValue(value === "all" ? "" : value)
+          }
+        >
+          <SelectTrigger className="w-[150px] sm:w-[180px]">
+            <SelectValue placeholder="Select mode" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="all">All transactions</SelectItem>
+            <SelectItem value="Income">Incomes</SelectItem>
+            <SelectItem value="Expense">Expenses</SelectItem>
+          </SelectContent>
+        </Select>
+        {isFiltered && (
+          <Button
+            variant="ghost"
+            onClick={() => table.resetColumnFilters()}
+            className="h-8 px-2 lg:px-3"
+          >
+            Reset
+            <CircleX className="ml-2 h-4 w-4" />
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
