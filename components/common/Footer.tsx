bd2de9e377cc1d43a55038270ef9d671b9464699
ColinAdo <colinadodev30@gmail.com>
2025-06-19 08:19:34 +0300

    Add proper footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="h-16">
@@ -7,6 +9,15 @@ export default function Footer() {
             &copy; 2025 Monie • All rights reserved.
           </p>
           <p className="text-gray-500 text-xs">inbox@mail.colinadore.tech</p>
+          <footer className="text-center text-sm mt-5 text-gray-500">
+            <Link href="/privacy-policy" className="hover:underline">
+              Privacy Policy
+            </Link>{" "}
+            •{" "}
+            <Link href="/terms-of-service" className="hover:underline">
+              Terms of Service
+            </Link>
+          </footer>
         </div>
       </div>
     </footer>
